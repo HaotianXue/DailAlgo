a79a300ddbd2babb475c7176b1f749d6fc223f90
HaoTianXue <haotianxue@HaoTianXuedeMacBook-Pro.local>
2021-02-24 15:22:22 -0500

    prefix_suffix_search added

--- prefix_suffix_search.cpp
+++ prefix_suffix_search.cpp
@@ -23,6 +23,7 @@ public:
         sufRoot = new TrieNode();
     }
     
+    // O(2l)
     void insert(string &word, int index) {
         TrieNode* node = preRoot;
         for (char &c: word) {
@@ -45,6 +46,7 @@ public:
         node->isWord = true;
     }
     
+    // O(2l + n)
     int getWordIndexByPreSuff(string& prefix, 
                               string& suffix) {
         TrieNode* node = preRoot;
@@ -84,6 +86,84 @@ public:
     
     int f(string prefix, string suffix) {
         return tree.getWordIndexByPreSuff(prefix, suffix);
+    }
+};
+
+
+
+// Approach 2: use ons trie to insert {suffix}_{prefix}
+// when we search with prefix: a and suffix: b, we directly search if "b{a" is a prefix of string stored in the trie
+// apple => {apple, e{apple, le{apple, ..., apple{apple
+// if we search e{a => it's a prefix of e{apple => we can get it from trie
+class TrieNode {
+public:
+    vector<TrieNode*> sons;
+    bool isWord;
+    int weight;
+    
+    TrieNode() {
+        sons = vector<TrieNode*>(27, NULL);
+        isWord = false;
+        weight = -1;
+    }
+    
+};
+
+class Trie {
+public:
+    TrieNode* root;
+    
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string& word, int index) {
+        TrieNode* node = root;
+        for (char& c: word) {
+            int offset = c - 'a';
+            if (!node->sons[offset]) node->sons[offset] = new TrieNode();
+            node = node->sons[offset];
+            // need to save the weight as we will search for prefix later
+            node->weight = index; // update index
+        }
+        node->isWord = true;
+    }
+    
+    int startWith(string& word) {
+        TrieNode* node = root;
+        for (char& c: word) {
+            int offset = c - 'a';
+            if (!node->sons[offset]) return -1;
+            node = node->sons[offset];
+        }
+        return node->weight;
+    }
+    
+};
+
+
+class WordFilter {
+private:
+    Trie tree;
+public:
+    // O(Nl^2)
+    WordFilter(vector<string>& words) {
+        // apple => {apple, e{apple, le{apple, ..., apple{apple
+        // if we search e{a => it's a prefix of e{apple => we can get it from trie
+        for (int i = 0; i < words.size(); ++i) {
+            string key = "{" + words[i];
+            tree.insert(key, i);
+            for (int j = words[i].size() - 1; j >= 0; --j) {
+                key = words[i][j] + key;
+                tree.insert(key, i);
+            }
+        }
+    }
+    
+    // O(l)
+    int f(string prefix, string suffix) {
+        string key = suffix + "{" + prefix;
+        return tree.startWith(key);
     }
 };
 
