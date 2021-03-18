ecd3e965b94a69fed43dee14eb4fa6a2694e7145
HaoTianXue <haotianxue@HaoTianXuedeMacBook-Pro.local>
2021-03-18 16:03:15 -0400

    meeting_scheduler added

--- meeting_scheduler.cpp
+++ meeting_scheduler.cpp
@@ -1,4 +1,41 @@
 // https://leetcode.com/problems/meeting-scheduler/
+
+// Same idea but simpler code
+class Solution {
+public:
+    vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
+        // find first two slots that intersect
+        // determine the length of intersect and compare it with duration
+        // if so => return
+        // otherwise the one finish eailer move to next slot
+        
+        auto cmp = [](const vector<int>& v1, const vector<int>& v2) {
+            if (v1[0] == v2[0]) return v1[1] < v2[1];
+            return v1[0] < v2[0];
+        };
+        
+        sort(slots1.begin(), slots1.end(), cmp);
+        sort(slots2.begin(), slots2.end(), cmp);
+        
+        int i = 0, j = 0;
+        int n1 = slots1.size(), n2 = slots2.size();
+        
+        
+        while (i < n1 && j < n2) {
+            vector<int> i1 = slots1[i], i2 = slots2[j];
+            int start = max(i1[0], i2[0]);
+            int end = min(i1[1], i2[1]);
+            if (end - start >= duration) { // this can tell if i1 and i2 intersects
+                return {start, start + duration};
+            } else { // no intersction: the one finish eailer move to next slot
+                if (i1[1] < i2[1]) ++i;
+                else ++j;
+            }
+        }
+        return {};
+    }
+};
+
 class Solution {
 public:
     vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
