eaa6cf4bdd2b544a90f8ac27cea9b8350296f19b
HaoTianXue <haotianxue@HaoTianXuedeMacBook-Pro.local>
2021-03-30 15:08:38 -0400

    coin change 2

--- coin_change_2.cpp
+++ coin_change_2.cpp
@@ -2,9 +2,24 @@
 
 class Solution {
 public:
-    // dp[i][j]: number of ways of first i coins to make up j amount
-    // dp[i][j] = dp[i - 1][j] + sum{dp[i - 1][j - k * coins[i]]}
-    int change_2D(int amount, vector<int>& coins) {
+    
+    // convert to 0-1 knapsnack
+    // dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]]
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                dp[i][j] += dp[i - 1][j] + ((j >= coins[i - 1]) ? dp[i][j - coins[i - 1]] : 0);
+            }
+        }
+        return dp[n][amount];
+    }
+    
+    int change_3loops(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
         for (int i = 0; i <= n; ++i) {
@@ -18,6 +33,25 @@ public:
             }
         }
         return dp[n][amount];
+    }
+    
+    // dp[i][j]: number of ways of first i coins to make up j amount
+    // dp[i][j] = dp[i - 1][j] + sum{dp[i - 1][j - k * coins[i]]}
+    int change_rolling(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(2, vector<int>(amount + 1, 0));
+        for (int i = 0; i <= n; ++i) {
+            dp[i % 2][0] = 1;
+        }
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                dp[i % 2][j] = 0; // must add, otherwise we actually add new stuff based on previous cell value(as we share cells)
+                for (int k = 0; k <= j / coins[i - 1]; ++k) {
+                    dp[i % 2][j] += dp[(i - 1) % 2][j - k * coins[i - 1]];
+                }
+            }
+        }
+        return dp[n % 2][amount];
     }
     
     // one-dim optimization: dp[j] = dp[j] + dp[j - coins[i]]
@@ -34,7 +68,7 @@ public:
     }
     
     // memo search
-    int change(int amount, vector<int>& coins) {
+    int change_memo(int amount, vector<int>& coins) {
         
         unordered_map<string, int> dp;
         
