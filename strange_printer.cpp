aeb8f358cc8ac412c130a91ef110a76506a52ce1
HaoTianXue <haotianxue@HaoTianXuedeMacBook-Pro.local>
2021-04-04 20:13:59 -0400

    strange printer

--- strange_printer.cpp
+++ strange_printer.cpp
@@ -2,6 +2,11 @@
 
 class Solution {
 public:
+    
+    // dp[i][j]: minimum number of turns to print from s[i] -> s[j]
+    // default behaviour: dp[i][j] = dp[i][k - 1] + 1
+    // if s[k] == s[j] => dp[i][j] = min(dp[i][k], dp[k + 1][j - 1]), i <= k < j
+    // init: dp[i][i] = 1, dp[i][j] = 0 (if i > j <=> empty string)
     int strangePrinter(string s) {
         int n = s.size();
         
@@ -9,6 +14,7 @@ public:
         
         function<int(int, int)> dfs = [&](int i, int j) {
             if (i > j) return 0;
+            if (i == j) return 1;
             if (dp[i][j] != -1) return dp[i][j];
             
             int res = dfs(i, j - 1) + 1;
