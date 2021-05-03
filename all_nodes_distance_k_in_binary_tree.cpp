b87f8330a2935f8de052142005badc3612e05374
HaoTianXue <haotianxue@HaoTianXuedeMacBook-Pro.local>
2021-05-03 12:17:17 -0400

    all nodes distance k in binary tree

--- all_nodes_distance_k_in_binary_tree.cpp
+++ all_nodes_distance_k_in_binary_tree.cpp
@@ -9,6 +9,52 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// Approach i: build graph + BFS
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
+        if (!root || !target) return {};
+        vector<int> res;
+        buildGraph(nullptr, root);
+        int d = 0;
+        queue<TreeNode*> q;
+        q.push(target);
+        unordered_set<TreeNode*> visited;
+        visited.insert(target);
+        
+        while (!q.empty() && d <= K) {
+            int s = q.size();
+            for (int i = 0; i < s; ++i) {
+                TreeNode* curNode = q.front(); q.pop();
+                if (d == K) res.push_back(curNode->val);
+                for (auto nextNode: graph[curNode]) {
+                    if (visited.count(nextNode)) continue;
+                    q.push(nextNode);
+                    visited.insert(nextNode);
+                }
+            }
+            ++d;
+        }
+                
+        return res;        
+    }
+private:
+    unordered_map<TreeNode*, vector<TreeNode*>> graph;
+    
+    void buildGraph(TreeNode* parent, TreeNode* child) {
+        if (parent) {
+            graph[parent].push_back(child);
+            graph[child].push_back(parent);
+        }
+        if (child->left) buildGraph(child, child->left);
+        if (child->right) buildGraph(child, child->right);
+    }
+    
+};
+
+
+// Approach ii: recurison
 class Solution {
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
