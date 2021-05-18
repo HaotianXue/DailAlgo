3b542d38cd76de9806084a90058e4297a6808986
HaoTianXue <haotianxue@HaoTianXuedeMacBook-Pro.local>
2021-05-17 23:11:19 -0400

    valid parenthesis string

--- valid_parenthesis_string.cpp
+++ valid_parenthesis_string.cpp
@@ -6,10 +6,10 @@ public:
         int N = 110;
         vector<vector<vector<int>>> memo(N, vector<vector<int>>(N, vector<int>(N, -1)));
         
-        function<bool(int, int, int)> check = [&](int start, int leftCount, int rightCount) {
+        function<bool(int, int, int)> check = [&](int start, int leftCount, int rightCount) -> bool {
             if (memo[start][leftCount][rightCount] != -1) return memo[start][leftCount][rightCount];
-            if (start == n) return memo[start][leftCount][rightCount] = leftCount == rightCount;
-            if (rightCount > leftCount) return memo[start][leftCount][rightCount] = false;
+            if (start == n) return leftCount == rightCount;
+            if (rightCount > leftCount) return false;
             if (s[start] == '(') return memo[start][leftCount][rightCount] = check(start + 1, leftCount + 1, rightCount);
             if (s[start] == ')') return memo[start][leftCount][rightCount] = check(start + 1, leftCount, rightCount + 1);
             
