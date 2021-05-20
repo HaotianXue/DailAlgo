a86bd9d19871bf560e59098a7335ed1af739172d
HaoTianXue <haotianxue@HaoTianXuedeMacBook-Pro.local>
2021-05-20 16:48:28 -0400

    different ways to add parenthese

--- different_ways_to_add_parentheses.cpp
+++ different_ways_to_add_parentheses.cpp
@@ -8,6 +8,8 @@ namespace Op {
 
 class Solution {
 public:
+    // 1 涉及括号组合类的一般要使用dfs。这里并不是真实添加括号，每个符号将表达式分为left,right两部分。对应的结果集合组合计算。
+    // 2 递归遍历left, right部分，直到表达式为纯数字。 对于 表达式exp，可以使用map缓存结果，递归遍历时可以快速返回结果
     vector<int> diffWaysToCompute(string expression) {
         if (cache.count(expression)) return cache[expression];
         vector<int> res;
