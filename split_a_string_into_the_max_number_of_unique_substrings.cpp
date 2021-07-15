2e51aec74470eccc42e3f96c2df78e08adb0006e
HaoTianXue <haotianxue@HaoTianXuedeMBP.domain>
2021-07-15 11:52:57 -0700

    leetcode 1593

--- split_a_string_into_the_max_number_of_unique_substrings.cpp
+++ split_a_string_into_the_max_number_of_unique_substrings.cpp
@@ -1,6 +1,38 @@
 https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/
 class Solution {
 public:
+
+    // 类似子集的两种实现方式
+
+    // method 1: 每次寻找以index开头的split方式
+    int maxUniqueSplit(string s) {
+        int n = s.size();
+        if (n <= 1) return n;
+        
+        unordered_set<string> set;
+        int res = 0;
+        
+        function<void(int)> dfs = [&](int index) {
+            if (index == n) {
+                res = max(res, (int)set.size());
+                return;
+            }
+            
+            for (int i = index; i < s.size(); ++i) {
+                string curS = s.substr(index, i - index + 1);
+                if (set.count(curS)) continue;
+                set.insert(curS);
+                dfs(i + 1);
+                set.erase(curS);
+            }
+            
+        };
+        
+        dfs(0);
+        return res;
+    }
+
+    // method 2: 当前index选或不选(split or not)
     int maxUniqueSplit(string s) {
         int n = s.size();
         if (n <= 1) return n;
